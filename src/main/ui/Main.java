f2a8aff6f904ee6761fe8b79f3137fa42b87f3fc
Stephanie Feng <sfeng19@students.cs.ubc.ca>
2023-03-25 23:40:59 -0700

    Added random restaurant GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,17 +6,6 @@ package ui;
 
 import ui.gui.RestaurantManagerApp;
 
-//public class Main {
-//    // EFFECTS: initializes new restaurant manager application
-//    public static void main(String[] args) {
-//        try {
-//            new ConsoleRestaurantManagerApp();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File not found, unable to run application.");
-//        }
-//    }
-//}
-
 public class Main {
     // EFFECTS: initializes new restaurant manager application
     public static void main(String[] args) {
